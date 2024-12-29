a0d775d170303f52b7e09ecd40d2dff54a8be12f
Ansh Jain <ajjain012617@gmail.com>
2024-12-30 02:55:09 +0530

    3rd

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,41 @@
-import { useState } from "react";
+import { useContext } from "react";
+import { CountContext } from "./context";
 
 
 function App() {
-  const [count,setCount]=useState(0);
+ 
   return (
     <div>
      <Count/>
-     <Buttons/>
     </div>
   );
 }
 
 function  Count(){
+  return <div>
+    <CountRenderer/>
+    <Buttons setCount={setCount}/>
+  </div>
 
 }
+function CountRenderer(){
+   const count=0;
+  return<div>
+    {count}
 
+  </div>
+}
 function Buttons(){
+ const count=0;
+  return <div>
+        <button onClick={()=>{
+          setCount(count+1)
+      }}>INCREASE</button>
 
+        <button onClick={()=>{
+          setCount(count-1)
+
+        }}> DECREASE </button>
+   </div>
 }
 export default App;
