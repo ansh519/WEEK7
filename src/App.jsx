bbc601985d7a310e72c833349cffde0ba3a265df
Ansh Jain <ajjain012617@gmail.com>
2024-12-29 14:55:53 +0530

    2nd

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,21 @@
-import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
-import './App.css';
-import React, { lazy, Suspense } from 'react';
+import { useState } from "react";
 
-// Lazy load components
-const Dashboard = lazy(() => import('./components/Dashboard'));
-const Landing = lazy(() => import('./components/Landing'));
 
 function App() {
+  const [count,setCount]=useState(0);
   return (
     <div>
-      <BrowserRouter>
-        <Appbar />
-        <Suspense fallback={<div>Loading...</div>}>
-          <Routes>
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/" element={<Landing />} />
-          </Routes>
-        </Suspense>
-      </BrowserRouter>
+     <Count/>
+     <Buttons/>
     </div>
   );
 }
 
-function Appbar() {
-  const navigate = useNavigate();
-  return (
-    <div>
-      <button
-        onClick={() => {
-          navigate('/');
-        }}
-      >
-        LANDING
-      </button>
+function  Count(){
 
-      <button
-        onClick={() => {
-          navigate('/dashboard');
-        }}
-      >
-        DASHBOARD
-      </button>
-    </div>
-  );
 }
 
+function Buttons(){
+
+}
 export default App;
